9169a49578998aa8785d21b1a452f7377b6d31ea
Stephen Hyde <steve1030uk@gmail.com>
2016-10-05 08:46:15 +0100

    Add DT2W, only half mode seems to work currently on the bottom half of display

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,6 +7,6 @@ mkdir output
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5nlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j9 -C $(pwd) O=output
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
 tools/dtbTool -s 2048 -o output/arch/arm/boot/dt.img -p output/scripts/dtc/ output/arch/arm/boot/dts/ -v
 cp output/arch/arm/boot/dt.img arch/arm/boot/dt.img
